86b769e6190ef90d014733eaee8be28252bd3487
martial69320 <ydserveur@gmail.com>
2016-11-11 22:09:23 +0100

    Update reini.cpp
    Retouche des anciennes lignes de codes vers les nouvelles (gossip)

--- src/server/scripts/Custom/customcerberus/reini.cpp
+++ src/server/scripts/Custom/customcerberus/reini.cpp
@@ -8,46 +8,46 @@
 class Reini_NPC : public CreatureScript
 {
 public:
-        Reini_NPC() : CreatureScript("Reini_NPC") { }
+    Reini_NPC() : CreatureScript("Reini_NPC")
+    {
+    }
  
-        bool OnGossipHello(Player *player, Creature *creature)
+    bool OnGossipHello(Player *player, Creature *creature)
+    {
+        player->PlayerTalkClass->ClearMenus();
+        AddGossipItemFor(player, 0, "Reinitialiser les instances", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+        SendGossipMenuFor(player, DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
+        return true;
+    }
+ 
+   bool OnGossipSelect(Player *player, Creature *creature, uint32 /*sender*/, uint32 action)
+    {
+        if (!player)
+            return true;
+ 
+        switch (action)
         {
-                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, "Reinitialiser les instances", GOSSIP_SENDER_MAIN, 73);
-                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, "Au revoir", GOSSIP_SENDER_MAIN, 74);
-                player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
-                return true;
-        }
-        bool OnGossipSelect(Player *player, Creature *creature, uint32 /*sender*/, uint32 action)
-        {
-                player->PlayerTalkClass->ClearMenus();
-                switch(action)
+            {
+        case GOSSIP_ACTION_INFO_DEF + 1:
+            for (uint8 i = 0; i < MAX_DIFFICULTY; ++i)
+            {
+                Player::BoundInstancesMap &binds = player->GetBoundInstances(Difficulty(i));
+                for (Player::BoundInstancesMap::iterator itr = binds.begin(); itr != binds.end();)
                 {
-                case 2: // Reset Instances
-                        for (uint8 i = 0; i < MAX_DIFFICULTY; ++i)
-                        {
-                                Player::BoundInstancesMap &binds = player->GetBoundInstances(Difficulty(i));
-                                for (Player::BoundInstancesMap::iterator itr = binds.begin(); itr != binds.end();)
-                                {
-                                        player->UnbindInstance(itr, Difficulty(i));
-                                }
-                        }
-						player->GetSession()->SendAreaTriggerMessage("Vos ID d'instances sont supprimer");
-                        player->CLOSE_GOSSIP_MENU();
-                        break;
-                case 73: // ...Back
-                        player->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, "Reinitialiser les instances", GOSSIP_SENDER_MAIN, 2);
-                        player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
-                        break;
-				case 74: // ...Back
-                        player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
-						player->CLOSE_GOSSIP_MENU();
-                        break;
+                    player->UnbindInstance(itr, Difficulty(i));
                 }
-                return true;
+            }
+            player->GetSession()->SendAreaTriggerMessage("Vos ID d'instances sont supprimer");
+            CloseGossipMenuFor(player);
+            break;
+            }
+            return true;
         }
+    }
+ 
 };
  
 void AddSC_Reini_NPC()
 {
-        new Reini_NPC();
+    new Reini_NPC();
 }

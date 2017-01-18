fbe06ae6f2cf73dd465f75f27e2cd598817325ff
martial69320 <ydserveur@gmail.com>
2017-01-18 15:25:18 +0100

    Update Player.cpp

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -26655,6 +26655,7 @@ void Player::RemoveRestFlag(RestFlag restFlag)
 void Player::SetSelection(ObjectGuid guid)
  {
    uint32 m_curSelection = guid;
+	void m_curSelection;
    SetUInt64Value(UNIT_FIELD_TARGET, guid);
  }
 
